3acd6626179a25dfc6de5b34a89c0953e4b1f7b8
esraa-98 <esraaabdallahmohammed@gmail.com>
2023-10-25 04:00:31 +0300

    todo files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+
+import AddTaskForm from './components/addTask.js';
+
+import ToDo from './components/toDoList.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
+  // Tasks (ToDo List) State
+  const [toDo, setToDo] = useState([]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+
+  // Add task 
+  const addTask = () => {
+    if(newTask) {
+      let num = toDo.length + 1; 
+      let newEntry = { id: num, title: newTask, status: false }
+      setToDo([...toDo, newEntry])
+      setNewTask('');
+    }
+  }
+
+  // Delete task 
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter( task => task.id !== id)
+    setToDo(newTasks);
+  }
+
+  // Mark task as done 
+  const markDone = (id) => {
+    let newTask = toDo.map( task => {
+      if( task.id === id ) {
+        return ({ ...task, status: !task.status })
+      }
+      return task;
+    })
+    setToDo(newTask);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+
+    <br /><br />
+    <h2>To Do List </h2>
+    <br /><br />
+
+      <AddTaskForm 
+        newTask={newTask}
+        setNewTask={setNewTask}
+        addTask={addTask}
+      />
+    
+
+    {/* Display ToDos */}
+
+    {toDo && toDo.length ? '' : 'No Tasks...'}
+
+    <ToDo
+      toDo={toDo}
+      markDone={markDone}
+      deleteTask={deleteTask}
+    />  
     </div>
   );
 }
 
-export default App;
+export default App
